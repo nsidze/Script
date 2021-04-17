4e38cbca7c82026a27a2de9ec789cf922b541bf1
nsidze <76187327+nsidze@users.noreply.github.com>
2021-04-17 11:17:39 -0400

    Create inventory.sh

---
+++ inventory.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Roro starting to have fun
+if[$? -eq 0]
+echo "success"
+else 
+echo "failed"
+fi 
+
+f_name = romaric
+
+if[ ${f_name}=romaric]
+echo " Welcome RORO"
+else 
+echo " Whee are you"
